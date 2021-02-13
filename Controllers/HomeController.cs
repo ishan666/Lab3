19b488b839b16107f53f342918687ffa7be8d385
Ishan <bori0013@algonquinlive.com>
2021-02-12 20:27:31 -0500

    Lab03 Finished

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        public IActionResult Count(Person person)
+        {
+            return View(person);
+        }
+
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress
+        {
+            get; 
+            set;
+        }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public string Date
+
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Description
+        {
+            get; 
+            set;
+        }
+
+        [Required]
+        public int Bottle
+        {
+            get; 
+            set;
+        }
+
+    }
+}
+
